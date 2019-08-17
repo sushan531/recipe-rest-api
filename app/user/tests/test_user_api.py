cd3ab00b82ba9743cc49de268e6876da2ed232b7
spark531 <sushan531@gmail.com>
2019-08-17 08:40:10 +0545

    added tests for token generation using rest framework built in token generator, added end point for token generation for users

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -6,10 +6,12 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
+
 
 class PublicUserApiTests(TestCase):
     """Test the users API (public)"""
@@ -17,8 +19,8 @@ class PublicUserApiTests(TestCase):
     def setUp(self) -> None:
         self.client = APIClient()
         self.payload = {
-            "email":"user1@gmail.com",
-            "password":"Admin@123",
+            "email": "user1@gmail.com",
+            "password": "Admin@123",
         }
 
     def test_create_valid_user_success(self):
@@ -26,10 +28,9 @@ class PublicUserApiTests(TestCase):
         response = self.client.post(CREATE_USER_URL, self.payload)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        user= get_user_model().objects.get(**response.data)
+        user = get_user_model().objects.get(**response.data)
         self.assertTrue(user.check_password(self.payload["password"]))
         self.assertNotIn("password", response.data)
-
 
     def test_user_exists(self):
         """Test to create user that already exists"""
@@ -37,11 +38,37 @@ class PublicUserApiTests(TestCase):
         response = self.client.post(CREATE_USER_URL, self.payload)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-
     def test_password_min_length(self):
         """Test for min password length"""
         self.payload["password"] = "pw"
         response = self.client.post(CREATE_USER_URL, self.payload)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email = self.payload["email"]).exists()
+        user_exists = get_user_model().objects.filter(email=self.payload["email"]).exists()
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """Test that a token is created for user"""
+        create_user(**self.payload)
+        response = self.client.post(TOKEN_URL, self.payload)
+        self.assertIn('token', response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid_creds(self):
+        """Test that the token is not created if the cres given are invalid"""
+        create_user(**self.payload)
+        self.payload["password"] = "pw"
+        response = self.client.post(TOKEN_URL, **self.payload)
+        self.assertNotIn("token", response.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_no_user(self):
+        response = self.client.post(TOKEN_URL, **self.payload)
+        self.assertNotIn("token", response.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_missing_input_field(self):
+        """Test that email and password are required"""
+        self.payload["password"] = None
+        response = self.client.post(TOKEN_URL, **self.payload)
+        self.assertNotIn("token", response.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- app/user/urls.py
+++ app/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, CreateTokenView
 
 app_name = "user"
 urlpatterns = [
     path("create/", CreateUserView.as_view(), name="create"),
+    path("token/", CreateTokenView.as_view(), name="token"),
 ]
