919aab9f67e06625af28d275859eb7683668be56
spark531 <sushan531@gmail.com>
2019-08-19 12:30:38 +0545

    CI demo

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+import csv
 
 from rest_framework.test import APIClient
 from rest_framework import status
