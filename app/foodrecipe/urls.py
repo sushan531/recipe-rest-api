26554cc470d2c35c482cf3ec1e918a58bb2d8876
sushan531@gmail.com <redTube4@Lifes>
2019-08-21 12:27:28 +0545

    typo fix

--- app/foodrecipe/urls.py
+++ app/foodrecipe/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path("api/recipe/",include("recipe.urls", namespace="recipe"))
 
 ]
-urlpatterns+=static(settings.MEDIAL_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
